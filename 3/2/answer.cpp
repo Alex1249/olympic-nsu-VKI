ec91fa13ee0c4abee0e488cad562102d9e0605e7
Alex1249 <57974798+Alex1249@users.noreply.github.com>
2022-09-25 22:33:09 +0700

    Update answer.cpp

---
+++ 3/2/answer.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+using namespace std;
+
+string OUTPUT = "";
+
+
+int get(char n)
+{
+    if (n < 87)
+        return n - '0';
+    else
+        return n - 87;
+}
+
+char get_char(int n)
+{
+    if (n < 10)
+        return n + '0';
+    else
+        return n + 87;
+}
+void CTTsd(std::string str, int base) {
+    int count = 0;
+    int rez = 0;
+    for (int i = str.length()-1; i >= 0; i--)
+    {
+        int n = get(str[i]);
+        int value = 0;
+        value = n * pow(base, count);
+        rez += value;
+        count++;
+    }
+    int tw = rez;
+    while (tw > 0) {
+        int value = tw % 10;
+        OUTPUT += get_char(value);
+        tw = tw / 10;
+    }
+    
+}
