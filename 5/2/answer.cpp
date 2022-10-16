420d00bba609aa23c7e52a01fa3261b8d5a3d27f
Alex1249 <57974798+Alex1249@users.noreply.github.com>
2022-10-16 22:21:30 +0700

    Update answer.cpp

---
+++ 5/2/answer.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <time.h>
+#include <stdio.h>
+using namespace std;
+
+int invtav_permut(int a[], int b[], int n) {
+    for (int i = 1; i <= n; i++) {
+        int spaceCount = 0;
+        for (int j = 0; j < n; j++) {
+            if (b[j] == 0 && spaceCount == a[i - 1]) {
+                b[j] = i;
+                break;
+            }
+            if (b[j] == 0) spaceCount++;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (b[i] == 0) return 0;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << b[i]<<" ";
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int* a = new int[n];
+    int* b = new int[n] {
+        0
+    };
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    if (!invtav_permut(a, b, n)) cout << "NO";
+    return 0;
+}
