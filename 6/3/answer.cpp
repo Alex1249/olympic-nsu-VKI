2f1e44f0782085f007dfd5c1d5817dc79274f3a7
Alex1249 <57974798+Alex1249@users.noreply.github.com>
2022-11-11 17:48:48 +0700

    Update answer.cpp

--- 6/3/answer.cpp
+++ 6/3/answer.cpp
@@ -3,30 +3,49 @@
 using namespace std;
 
 
-void f(float* a, int n, int x) {
-	int b, c, d;
-	b = 0;
-	c = n - 1;
-	while (1 < (c - b)) {
-		d = (c + b) / 2;
-		if (a[d] < x) b = d;
-		else c = d;
+void undstr(string a, string a1, int* b) {
+	int sch, p[100001] = { 0 }, x = 0, i = 0, n = a.length(), n1 = a1.length();
+	while (i <= n1 - n)
+	{
+		if (a[n - 1] == a1[i + n - 1])
+		{
+			sch = n - 1;
+			while (sch >= 0 && a[sch] == a1[i + sch]) {
+				sch--;
+			}
+			if (sch == -1) {
+				p[x] = i;
+				x++;
+			}
+		}
+		i += b[a1[i + (n - 1)]];
 	}
-	if (x - a[b] <= a[c] - x) cout << b;
-	else cout << c;
+	cout << x << "\n";
+	for (int i = 0; i <= x - 1; i++) {
+		cout << p[i] << "\n";
+	}
 }
 
 
 int main() {
-	int n, x;
-	float* a;
-	cin >> n >> x;
-	a = (float*)malloc(sizeof(float) * n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i];
+	string a = "", a1 = "";
+    cin >> a1;
+    cin >> a;
+
+    int b[256] = { 0 }, h = 1;
+
+	int n = a.length();
+	for (int & i : b) {
+		i = n;
 	}
-	f(a, n, x);
-	free(a);
-	return 0;
+
+	for (int i = n - 2; i >= 0; i--) {
+		if (b[a[i]] == n) {
+			b[a[i]] = h;
+		}
+		h++;
+	}
+	undstr(a, a1, b);
+
+    return 0;
 }
