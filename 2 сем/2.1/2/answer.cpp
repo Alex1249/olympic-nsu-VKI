73560d4eefbe9abdb36e8f51c68ee331c7e88c9b
Alex1249 <57974798+Alex1249@users.noreply.github.com>
2023-03-04 15:11:32 +0700

    Update answer.cpp

---
+++ 2 сем/2.1/2/answer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+using namespace std;
+int readTime ( char * iStr , int * oHours , int * oMinutes , int * oSeconds ){
+    int h = 0, m = 0, s = 0;
+    int match = sscanf(iStr,"%d:%d:%d", &h, &m, &s);
+    if (match > 1 and h < 24 and m < 60 and s < 60){
+        *oHours = h;
+        *oSeconds = s;
+        *oMinutes = m;
+        return 1;
+    }
+    else{
+        *oHours = -1;
+        *oSeconds = -1;
+        *oMinutes = -1;
+        return 0;
+    }
+}
+ 
+int main() {
+    int h = 0, m = 0, s = 0;
+    char iTime[15];
+    scanf("%s", iTime);
+    int result = readTime(iTime, &h, &m, &s);
+    cout << result << " " << h << " " << m << " " << s << endl
+        << result << " " << h << " " << m << endl
+        << result << " " << h;
+    return 0;
+}
